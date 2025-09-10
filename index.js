6426140c1ab752f22a93e3aa8f4d77b3b4d07ee6
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-09-10 08:21:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// Import the app from /dev-server/index.js
-const app = require('./dev-server/index.js');
-
-// Run the app
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-  });
+const app = require("../index");
+module.exports = app;
